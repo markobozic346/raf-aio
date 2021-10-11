93a23c1c68e3c2ae2985bf8fbf5c12a358038b2a
Marko Božić <55995892+markobozic346@users.noreply.github.com>
2021-10-11 15:35:46 +0200

    Fetch data from google script scrapper

---
+++ src/services/scheduleService.ts
@@ -0,0 +1,25 @@
+class ScheduleService {
+  async fetchScheduleData() {
+    const url =
+      "https://script.google.com/macros/s/AKfycbx-IHfmCLu265z22d38-A-F-ZcQEBSmBesbOa1Z-d8dnZfzoPW36tIvQWygvjRF3hr7/exec";
+
+    return fetch(url)
+      .then(function (response) {
+        if (response.status !== 200) {
+          console.log(
+            "Looks like there was a problem. Status Code: " + response.status
+          );
+          return;
+        }
+
+        // Examine the text in the response
+        const jsonData = response.json();
+        return jsonData.then((data) => data);
+      })
+      .catch(function (err) {
+        console.log("Fetch Error :-S", err);
+      });
+  }
+}
+
+export const scheduleService = new ScheduleService();
