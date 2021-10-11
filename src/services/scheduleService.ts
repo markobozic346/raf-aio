b6b003031c12ec8dbcdbda87da6902d91ab80303
Marko Božić <55995892+markobozic346@users.noreply.github.com>
2021-10-11 16:45:14 +0200

    Organize data to array of objects

--- src/services/scheduleService.ts
+++ src/services/scheduleService.ts
@@ -1,10 +1,34 @@
 class ScheduleService {
+  async organizeData(data: any) {
+    const rawData = await data;
+
+    const tableHeaderRow = rawData && rawData.values[0];
+
+    //remove rows that are not schedule data
+    rawData.values.shift();
+    rawData.values.shift();
+
+    const organizedData = rawData.values.map((lecture: any) => ({
+      subject: lecture[0],
+      year: lecture[1],
+      group: lecture[2],
+      zoomUrl: lecture[3],
+      eLearningUrl: lecture[4],
+      info: lecture[5],
+    }));
+
+    return {
+      headings: tableHeaderRow,
+      ...organizedData,
+    };
+  }
+
   async fetchScheduleData() {
     const url =
       "https://script.google.com/macros/s/AKfycbx-IHfmCLu265z22d38-A-F-ZcQEBSmBesbOa1Z-d8dnZfzoPW36tIvQWygvjRF3hr7/exec";
 
     return fetch(url)
-      .then(function (response) {
+      .then((response) => {
         if (response.status !== 200) {
           console.log(
             "Looks like there was a problem. Status Code: " + response.status
@@ -12,11 +36,10 @@ class ScheduleService {
           return;
         }
 
-        // Examine the text in the response
-        const jsonData = response.json();
-        return jsonData.then((data) => data[0]);
+        // Return data
+        return response.json().then((data) => this.organizeData(data[0]));
       })
-      .catch(function (err) {
+      .catch((err) => {
         console.log("Fetch Error :-S", err);
       });
   }
