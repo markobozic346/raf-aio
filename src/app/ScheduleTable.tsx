218567ade74cfbbef3218062c57ffe19644e280a
Marko Božić <55995892+markobozic346@users.noreply.github.com>
2021-10-12 11:21:36 +0200

    Add react-table and show data in table

--- src/app/ScheduleTable.tsx
+++ src/app/ScheduleTable.tsx
@@ -3,6 +3,7 @@ import { Box } from "@chakra-ui/react";
 
 import { scheduleService } from "../services/scheduleService";
 import MyTable from "../components/Table";
+import React from "react";
 
 interface Props {}
 
@@ -12,13 +13,44 @@ const ScheduleTable = (props: Props) => {
   useEffect(() => {
     getScheduleData();
   }, []);
-
+  console.log(data);
   const getScheduleData = async () => {
     const temp = await scheduleService.fetchScheduleData();
     setData(temp);
   };
-
-  return <Box>{data && <MyTable tData={data} />}</Box>;
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: data?.headings[0],
+        accessor: "subject",
+      },
+      {
+        Header: data?.headings[1],
+        accessor: "year",
+      },
+      {
+        Header: data?.headings[2],
+        accessor: "group",
+      },
+      {
+        Header: data?.headings[3],
+        accessor: "zoomUrl",
+        Cell: ({ cell }: any) => <a href={cell.value}>{cell.value}</a>,
+      },
+      {
+        Header: data?.headings[4],
+        accessor: "eLearningUrl",
+      },
+      {
+        Header: data?.headings[5],
+        accessor: "info",
+      },
+    ],
+    [data]
+  );
+  return (
+    <Box>{data && <MyTable columns={columns} data={data.scheduleData} />}</Box>
+  );
 };
 
 export default ScheduleTable;
