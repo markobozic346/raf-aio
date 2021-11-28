78da6ade60eeb6542a2e90f17c4d62ff1a0c121b
Marko Bozic <markobozic346@gmail.com>
2021-11-28 21:14:30 +0100

    Format

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -4,26 +4,26 @@ import { useScheduleContext } from "../app/home/schedule-context";
 
 const SearchInput = () => {
   const [savedSearch, setSavedSearch] = useState<string | undefined | null>();
-  
+
   const { onSearch } = useScheduleContext();
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     onSearch(e.target.value);
-    localStorage.setItem('group' ,e.target.value)
+    localStorage.setItem("group", e.target.value);
   };
   useEffect(() => {
-    const group = localStorage.getItem('group')
-    setSavedSearch(group)
-  }, [])
+    const group = localStorage.getItem("group");
+    setSavedSearch(group);
+  }, []);
 
-  savedSearch && onSearch(savedSearch)
+  savedSearch && onSearch(savedSearch);
 
   return (
     <Box p="20px">
       <Input
         w="100%"
         placeholder="unesi ime grupe npr 1s1"
-        defaultValue={savedSearch ? savedSearch : ''}
+        defaultValue={savedSearch ? savedSearch : ""}
         onChange={handleChange}
       />
     </Box>
