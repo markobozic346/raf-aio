8e9149a645b5e745c37c273b7cc29ae6bf7db680
Marko Bozic <markobozic346@gmail.com>
2021-11-28 21:11:43 +0100

    Save search to local storage

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,19 +1,29 @@
-import { ChangeEvent } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { Box, Input } from "@chakra-ui/react";
 import { useScheduleContext } from "../app/home/schedule-context";
 
 const SearchInput = () => {
+  const [savedSearch, setSavedSearch] = useState<string | undefined | null>();
+  
   const { onSearch } = useScheduleContext();
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     onSearch(e.target.value);
+    localStorage.setItem('group' ,e.target.value)
   };
+  useEffect(() => {
+    const group = localStorage.getItem('group')
+    setSavedSearch(group)
+  }, [])
+
+  savedSearch && onSearch(savedSearch)
 
   return (
     <Box p="20px">
       <Input
         w="100%"
         placeholder="unesi ime grupe npr 1s1"
+        defaultValue={savedSearch ? savedSearch : ''}
         onChange={handleChange}
       />
     </Box>
